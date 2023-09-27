25023d9ffd22bffdc069d507ed133d1a64439164
Abderrazak B. <bouadma.abderrazak@gmail.com>
2023-09-27 21:02:02 +0200

    using chocolately to install packages

--- scripts/user-data.ps1
+++ scripts/user-data.ps1
@@ -1,11 +1,19 @@
-# Download and install GitLab Runner
-Invoke-WebRequest -Uri https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-windows-amd64.exe -OutFile C:\GitLab-Runner\gitlab-runner.exe
-Set-Service -Name gitlab-runner -StartupType 'Manual'
-Start-Service -Name gitlab-runner
+# Install Chocolatey if not already installed
+if (!(Test-Path 'C:\ProgramData\chocolatey\chocolateyinstall')) {
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
 
-# Register GitLab Runner with your GitLab instance
-# Replace <YOUR_REGISTRATION_TOKEN> with your actual registration token
-gitlab-runner register -n --url "https://gitlab.com/" --registration-token "<YOUR_REGISTRATION_TOKEN>" --executor "shell" --description "My Runner"
+# Refresh the environment variables to make Chocolatey available
+refreshenv
+
+# Install GitLab Runner using Chocolatey
+choco install gitlab-runner -y
+
+# Install Rust and Cargo using Chocolatey
+choco install rust -y
+
+# Add Rust and Cargo bin directories to the PATH
+[Environment]::SetEnvironmentVariable("PATH", $env:PATH + ";C:\Program Files\Rust\bin", [System.EnvironmentVariableTarget]::Machine)
 
 # Optionally, configure additional Runner settings if needed
 # gitlab-runner.exe edit
